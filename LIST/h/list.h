e04915100e76519ccc45a1b08f1f046c4cd108f0
Dušan Krdžić <77269536+DusanKrdzic@users.noreply.github.com>
2021-03-10 10:23:38 +0100

    Add files via upload

--- LIST/h/list.h
+++ LIST/h/list.h
@@ -25,10 +25,13 @@ public:
     public:
         Iterator(Node *p) : current(p) {}
 
-        Iterator(const Iterator&);
-        Iterator(Iterator&&);
+        Iterator(const Iterator &);
+        Iterator(Iterator &&);
+        Iterator &operator=(const Iterator &);
+        Iterator &operator=(Iterator &&);
 
         bool operator!=(const Iterator &);
+        bool operator==(const Iterator &); //ako overload operator!= trebalo bi i operator==
 
         int &operator*();
 
@@ -39,10 +42,11 @@ public:
 
     List();
 
-    List(const List&);
-    List(List&&);
+    List(const List &);
+    List(List &&);
 
-
+    List &operator=(const List &);
+    List &operator=(List &&);
 
     List &push_back(const int &);
 
